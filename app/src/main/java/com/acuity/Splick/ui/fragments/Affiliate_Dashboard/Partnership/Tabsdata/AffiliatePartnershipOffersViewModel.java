f189463e596f97822d0b2d0a4c3de832fae30168
Asus <alamgir@gmail.com>
2020-03-16 15:35:56 +0500

    Initial commit

---
+++ app/src/main/java/com/acuity/Splick/ui/fragments/Affiliate_Dashboard/Partnership/Tabsdata/AffiliatePartnershipOffersViewModel.java
@@ -0,0 +1,7 @@
+package com.acuity.Splick.ui.fragments.Affiliate_Dashboard.Partnership.Tabsdata;
+
+import androidx.lifecycle.ViewModel;
+
+public class AffiliatePartnershipOffersViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/acuity/Splick/ui/fragments/Affiliate_Dashboard/Partnership/Tabsdata/StatisticsData/StatisticsFor1DViewModel.java
@@ -0,0 +1,7 @@
+package com.acuity.Splick.ui.fragments.Affiliate_Dashboard.Partnership.Tabsdata.StatisticsData;
+
+import androidx.lifecycle.ViewModel;
+
+public class StatisticsFor1DViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
